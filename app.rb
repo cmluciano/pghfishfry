9c8d758c7cc5b036408f38638a9da1000155ab9e
drag00n <christopher.luciano@snapretail.com>
2014-03-04 00:34:59 -0500

    First Commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'sinatra'
+require 'yaml'
+
+get '/' do
+    erb :index
+end
+
+get '/about' do
+  @title = 'About Awesome Site'
+  erb :about
+end
+
+get '/fry/:location' do
+  file = YAML::load_file(File.join(__dir__,'data/frys.yml'))
+  @fry=file[params[:location]]
+  erb :fry
+end
+
+
